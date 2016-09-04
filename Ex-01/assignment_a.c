e0ca544313e096f50f52d384a1d036c78ac8d33b
Torbjørn Viem Ness <torbjorn@viem-it.no>
2016-09-04 22:06:10 +0200

    Added malloc error detection and print (completed assignment C)

--- Ex-01/assignment_a.c
+++ Ex-01/assignment_a.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 void allocate(int value) {
 	int *ptr = NULL;
 	ptr = malloc(100000 * sizeof(int));
+	if (ptr == NULL) {
+		perror("Failed to allocate memory: ");
+		exit(errno);
+	}
 	*ptr = value;
 	printf("test of allocated memory: %i\n", ptr);
 }
