69429794ad9bc405d09ec24b31db9c4ac98a0343
Ballefrans von Dinkelbein <ballefrans@dinkelbein.org>
2016-10-07 12:09:45 +0200

    Finished Ex05

--- Ex-05/iotest.c
+++ Ex-05/iotest.c
@@ -10,6 +10,9 @@
 #define CHAN_B 2
 #define CHAN_C 3
 
+#define NUM_DISTURBANCE_THREADS 10
+#define TEST_PERIOD_US 100
+
 bool running = true;
 
 int set_cpu(int cpu_number) {
@@ -21,10 +24,20 @@ int set_cpu(int cpu_number) {
 	return pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t),&cpu);
 }
 
+void timespec_add_us(struct timespec *t, long us) {
+	t->tv_nsec += us*1000;
+	if (t->tv_nsec > 1000000000) {
+		t->tv_nsec = t->tv_nsec - 1000000000;
+		t->tv_sec += 1;
+	}
+}
+
 /** Function to monitor the inputs and respond */
 void* monitor_input(int* args) {
 	set_cpu(1);
 	int channel = args[0];
+	struct timespec next;
+	clock_gettime(CLOCK_REALTIME, &next);
 	printf("Worker for input %d ready and listening\n", channel);
 	while(running) {
 		// Poll the input and respond
@@ -33,7 +46,31 @@ void* monitor_input(int* args) {
 			usleep(5);
 			io_write(channel, 1);
 		}
-		usleep(10);
+		pthread_yield();
+//		usleep(10);
+//		timespec_add_us(&next, TEST_PERIOD_US);
+//		clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &next, NULL);
+	}
+}
+
+/** Function to waste cycles and disturb the main task */
+void* disturbance_task(int* args) {
+	set_cpu(1);
+	int channel = args[0];
+	printf("Disturbance thread %d running\n", channel);
+	while(running) {
+		1 + 1;
+	}
+}
+
+/** Function to say hello every 500ms */
+void* nag_task(int* args) {
+	struct timespec next;
+	clock_gettime(CLOCK_REALTIME, &next);
+	while (running) {
+		timespec_add_us(&next, 500000);
+		puts("Hello");
+		clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &next, NULL);
 	}
 }
 
@@ -44,17 +81,25 @@ int main(void) {
 	}
 	printf("I/O card initialized, current values: %d, %d, %d\n", io_read(CHAN_A), io_read(CHAN_B), io_read(CHAN_C));
 	for (int i = 1; i <= 3; i++) {
-		sleep(1);
-		io_write(i, !io_read(i));
-		printf("Changed value of channel %d to %d\n", i, io_read(i));
+		io_write(i,1); 
+		printf("Changed value of channel %d to 1\n", i);
 	}
-	
-	pthread_t t_id[3];
+
+	pthread_t t_id[14];
 	const int A = CHAN_A, B = CHAN_B, C = CHAN_C;
 	pthread_create(&t_id[0], NULL, &monitor_input, &A);
 	pthread_create(&t_id[1], NULL, &monitor_input, &B);
 	pthread_create(&t_id[2], NULL, &monitor_input, &C);
-	pthread_join(t_id[0], NULL);
-	pthread_join(t_id[2], NULL);
-	pthread_join(t_id[3], NULL);
+	for (int i = 0; i < NUM_DISTURBANCE_THREADS; i++) {
+		pthread_create(&t_id[i+3], NULL, &disturbance_task, &i);
+		usleep(5000);
+	}
+	pthread_create(&t_id[13], NULL, &nag_task, NULL);
+	pthread_join(&t_id[0], NULL);
+	pthread_join(&t_id[2], NULL);
+	pthread_join(&t_id[3], NULL);
+	for (int i = 0; i < 10; i++) {
+		pthread_join(&t_id[i+3], NULL);
+	}
+	pthread_join(&t_id[13], NULL);
 }
